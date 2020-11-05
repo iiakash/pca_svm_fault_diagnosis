0b108ce6070345f1a9008ac983a58b799ead3cfd
Intekhab Islam <inte.islam93@gmail.com>
2020-11-06 00:19:24 +0100

    remove summary function

--- fault_diagnosis_pca_svm.py
+++ fault_diagnosis_pca_svm.py
@@ -138,9 +138,3 @@ from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
 sc_Y = StandardScaler()
 X = sc_X.fit_transform(X)
-
-import statsmodels.formula.api as sm
-X = np.append(arr = np.ones((960, 1)).astype(int), values = X, axis = 1)
-X_opt = X[:, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52]]
-regressor_OLS = sm.OLS(endog = Y, exog = X_opt).fit()
-regressor_OLS.summary()'''
